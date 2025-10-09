2562418b090bb91aa23c7049aaff59ea03bec1f3
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-10-09 10:39:05 +0300

    Add sidebar menu on mobile.

--- app/sections/header/Header.tsx
+++ app/sections/header/Header.tsx
@@ -1,8 +1,15 @@
 "use client";
-import { Facebook, Instagram, LinkedIn } from "@mui/icons-material";
+import {
+  Close,
+  Facebook,
+  Instagram,
+  KeyboardArrowRight,
+  LinkedIn,
+  Menu,
+} from "@mui/icons-material";
 import { LocalPhone, Email, WhatsApp, AccessAlarm } from "@mui/icons-material";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { getContacts, getNavLinks, socials, Messages } from "@/lib/header";
 
 type HeaderProps = { isClient: boolean; messages: Messages };
@@ -154,40 +161,80 @@ export const Banner = ({ messages }: HeaderProps) => {
 
 export const Navbar = ({ messages }: HeaderProps) => {
   const navLinks = getNavLinks(messages);
+  const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
-    <nav className="between w-full h-[5rem] bg-[var(--primary-3)] px-4">
+    <nav className="between relative w-full h-[4rem] bg-[var(--primary-3)] px-4 z-5">
       <div className="flex items-center gap-3">
-        <button type="button" className="center md:hidden!">
-          🟰
+        <button
+          type="button"
+          className="center lg:hidden!"
+          onClick={() => setOpen(!isOpen)}
+        >
+          <Menu />
         </button>
         <span className="font-semibold">{messages["nav.brand"]}</span>
       </div>
-      <ul className="hidden md:flex items-center gap-6">
+      <ul className="center hidden! lg:flex! items-center gap-6">
         {navLinks.map((l) => (
           <li key={l.id}>
-            <Link href={l.href}>{l.label}</Link>
+            <Link
+              href={l.href}
+              className="nav-link center min-w-max relative px-4 z-5 hover:h-10 hover:text-[var(--primary-5)] "
+            >
+              {l.label}
+            </Link>
           </li>
         ))}
-      </ul>{" "}
-      <ul className="center">
+      </ul>
+      <ul className="center gap-4">
         <li>
           <Link
             href={`tel:${(messages["header.banner.phone"] || "").replace(
               /\s/g,
               ""
             )}`}
-            className="center h-12 min-w-[10rem] w-full bg-[var(--neutral-6)]"
+            className="center h-10 min-w-[10rem] w-full bg-[var(--neutral-6)] text-[var(--primary-3)] rounded-lg"
           >
             {messages["header.cta.call"]}
           </Link>
         </li>
-        <li>
+        <li className="center gap-2 h-10 border border-[var(--border-dark)] px-4 rounded-lg">
           <button type="button">{messages["nav.locale.fi"]}</button>
-          <span className="mx-1">/</span>
+
           <button type="button">{messages["nav.locale.en"]}</button>
         </li>
       </ul>
+      {isOpen && (
+        <div className="absolute top-full left-0 w-full min-h-[calc(100dvh-4rem)] bg-[var(--overlay)] z-1"></div>
+      )}
+      {isOpen && (
+        <div className="absolute top-0 left-0 w-full sm:w-[40vw] min-h-dvh bg-[var(--neutral-0)] z-10 sm:border-r-2 border-[var(--primary-4)]">
+          <div className="center justify-start! w-full h-[4rem] bg-[var(--primary-3)] px-4">
+            <span className="text-3xl">Lumi</span>
+          </div>
+          <ul className="flex flex-col w-full gap-4 py-5 divide-y-1 divide-[var(--primary-2)]">
+            {navLinks.map((l) => (
+              <li key={l.id} className="w-full not-last:pb-4 px-4">
+                <Link
+                  href={l.href}
+                  className="center justify-between! relative z-5 hover:text-[var(--primary-4)] text-xl font-semibold"
+                >
+                  {l.label}
+                  <KeyboardArrowRight className="text-[var(--primary-6)]" />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            onClick={() => setOpen(!isOpen)}
+            className="center h-8 w-8 absolute right-4 sm:-right-4 top-4 bg-[var(--primary-6)] rounded-full border border-[var(--border-dark)] shadow-2xl"
+          >
+            <Close />
+          </button>
+        </div>
+      )}
     </nav>
   );
 };
