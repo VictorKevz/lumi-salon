704d0942f919713b14091e6a5b710b437bb52ed2
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-10-11 21:01:02 +0300

    Add AnimationWrapper for scalability.

--- app/sections/Hero/Hero.tsx
+++ app/sections/Hero/Hero.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { FadeInVariants } from "@/app/variants";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 import { CTALink } from "@/app/components/CTALink";
 import { formatPhoneNumber } from "@/lib/format";
+import { ArrowDownward, KeyboardArrowDown } from "@mui/icons-material";
+import { AnimationWrapper } from "@/app/components/AnimationWrapper";
 
 type Messages = Record<string, string>;
 
@@ -17,46 +18,82 @@ export const Hero = ({
 }) => {
   if (!isClient) {
     return (
-      <section id="home" className="relative w-full min-h-screen">
-        <header>
-          <h1>{messages["hero.title"]}</h1>
-          <p>{messages["hero.subtitle"]}</p>
+      <section
+        id="home"
+        aria-label="hero section"
+        className="center flex-col! items-start! w-full min-h-[calc(100dvh-4.5rem)] lg:min-h-[calc(100dvh-7.2rem)] relative py-10 px-4 lg:px-8 z-5"
+      >
+        <header className="w-full">
+          <h1 className="text-3xl lg:text-6xl text-[var(--text-primary)] max-w-2xl">
+            {messages["hero.title"]}
+          </h1>
+          <p className="max-w-xl text-[var(--text-scondary)]">
+            {messages["hero.subtitle"]}
+          </p>
         </header>
-        <picture className="absolute top-0 left-0 w-full h-full object-cover object-center inset-0 -z-2 pointer-events-none">
+        <picture className="absolute top-0 left-0 w-full h-full inset-0 -z-2 pointer-events-none">
           <source
             media="(min-width:1024px )"
             srcSet="/images/hero-desktop.png"
           />
-          <img src="/images/hero-mobile.png" alt="" className="w-full h-auto" />
+          <img
+            src="/images/hero-mobile.png"
+            alt="Salon interior showing modern styling stations and elegant decor"
+            className="w-full h-full object-cover"
+          />
         </picture>
-        <div className="gradient-overlay backdrop-saturate-120 backdrop-brightness-70 -z-1"></div>
+        <div className="w-full max-w-md flex flex-col sm:flex-row gap-4 mt-6">
+          <CTALink
+            href={`tel:${formatPhoneNumber(messages["phone"])}`}
+            variant="fill"
+          >
+            {messages["header.cta.call"]}
+          </CTALink>
+          <CTALink href="#contact" variant="outline">
+            {messages["contact.title"]}
+          </CTALink>
+        </div>
+        <Link
+          href={"#services"}
+          className="center w-12 h-12 absolute bottom-4 rounded-full backdrop-blur-2xl border-2 border-[var(--border-dark)] text-[var(--neutral-6)]"
+        >
+          <span className="sr-only">{messages["cta.services"]}</span>
+          <span aria-hidden="true">
+            <ArrowDownward fontSize="large" />
+          </span>
+        </Link>
+        <div className="gradient-overlay backdrop-blur-[3px] lg:backdrop-blur-none backdrop-saturate-150 backdrop-brightness-50 -z-1"></div>
       </section>
     );
   }
   return (
     <section
       id="home"
-      className="center flex-col! items-start! w-full min-h-[calc(100dvh-4.5rem)] lg:min-h-[calc(100dvh-7.2rem)] relative py-10 px-4 lg:px-8 z-5"
+      aria-label="hero section"
+      className="center flex-col! items-start! w-full min-h-[calc(100dvh-8.5rem)] lg:min-h-[calc(100dvh-7.2rem)] relative py-10 px-4 lg:px-8 z-5"
     >
       <header className="w-full">
-        <motion.h1
+        <AnimationWrapper
+          as={motion.h1}
+          offset={10}
           className="text-3xl lg:text-6xl text-[var(--text-primary)] max-w-2xl"
-          variants={FadeInVariants(-10, 0)}
-          initial="hidden"
-          animate="visible"
         >
           {messages["hero.title"]}
-        </motion.h1>
-        <motion.p
+        </AnimationWrapper>
+        <AnimationWrapper
+          as={motion.p}
+          offset={10}
+          delay={0.2}
           className="max-w-xl text-[var(--text-scondary)]"
-          variants={FadeInVariants(10, 0.2)}
-          initial="hidden"
-          animate="visible"
         >
           {messages["hero.subtitle"]}
-        </motion.p>
+        </AnimationWrapper>
       </header>
-      <div className="w-full max-w-md flex flex-col sm:flex-row gap-4 mt-6">
+      <AnimationWrapper
+        offset={10}
+        delay={0.3}
+        className="w-full max-w-md flex flex-col sm:flex-row gap-4 mt-6"
+      >
         <CTALink
           href={`tel:${formatPhoneNumber(messages["phone"])}`}
           variant="fill"
@@ -66,16 +103,35 @@ export const Hero = ({
         <CTALink href="#contact" variant="outline">
           {messages["contact.title"]}
         </CTALink>
-      </div>
+      </AnimationWrapper>
+      <Link
+        href={"#services"}
+        className="center min-w-13 min-h-13 lg:w-18 lg:h-18 absolute bottom-4 rounded-full bg-[var(--primary-6)] hover:bg-[var(--neutral-6)] text-[var(--neutral-3)]"
+      >
+        <span className="sr-only">{messages["cta.services"]}</span>
+        <motion.span
+          aria-hidden="true"
+          animate={{
+            y: [0, 9, 0],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+        >
+          <KeyboardArrowDown className="text-3xl! lg:text-5xl!" />
+        </motion.span>
+      </Link>
       <picture className="absolute top-0 left-0 w-full h-full inset-0 -z-2 pointer-events-none">
         <source media="(min-width:1024px )" srcSet="/images/hero-desktop.png" />
         <img
           src="/images/hero-mobile.png"
-          alt=""
+          alt="Salon interior showing modern styling stations and elegant decor"
           className="w-full h-full object-cover"
         />
       </picture>
-      <div className="gradient-overlay backdrop-saturate-120 backdrop-brightness-70 -z-1"></div>
+      <div className="gradient-overlay backdrop-blur-[3px] lg:backdrop-blur-none backdrop-saturate-150 backdrop-brightness-50 -z-1"></div>
     </section>
   );
 };
